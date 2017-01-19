7effb028117115ce8159dff0de8d8eca7381af12
yasu <work.yasuyasu.1001.1720@gmail.com>
2017-01-19 12:05:59 +0900

    コンテキスト修正

--- index.php
+++ index.php
@@ -98,6 +98,7 @@ foreach ($events as $event) {
   $redis->expire('memory',100);
   //$context = $redis->get('context');
   error_log("-------- message start --------");
+  error_log($context);
   error_log($response->utt);
   error_log($response->context);
   error_log("-------- message end --------");
